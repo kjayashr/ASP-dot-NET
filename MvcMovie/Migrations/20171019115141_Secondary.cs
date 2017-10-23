f99cdf79c1c081fe033ec71be8b35257292174ac
kjayashr <kkanthjc@gmail.com>
2017-10-23 02:10:17 -0700

    Dockerized the app

---
+++ MvcMovie/Migrations/20171019115141_Secondary.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MvcMovie.Migrations
+{
+    public partial class Secondary : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lead",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    LeadName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Quote = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lead", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lead");
+        }
+    }
+}
---
+++ MvcMovie/Models/Lead.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcLead.Models
+{
+    public class Lead
+    {
+        public int ID { get; set; }
+        public string LeadName { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Type { get; set; }
+        public decimal Quote { get; set; }
+    }
+}
